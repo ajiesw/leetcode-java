d0853c4826aafabdca5bcac268574c36c72448a6
wangkj6 <wangkj6@asiainfo.com>
2023-03-08 18:11:12 +0800

    全排列 对数组进行分割83%

--- leetcode/src/main/java/com/kuer/leetcode/editor/cn/Permutations.java
+++ leetcode/src/main/java/com/kuer/leetcode/editor/cn/Permutations.java
@@ -56,16 +56,17 @@ public class Permutations {
         public List<List<Integer>> permute(int[] nums) {
             List<List<Integer>> ans = new ArrayList<>();
             List<Integer> paths = new ArrayList<>(nums.length);
-            myMethod(nums, paths, ans);
+//            myMethod(nums, paths, ans);
+            optimizeMyMethod(nums, paths, ans, 0);
             return ans;
         }
 
-        private void myMethod(int[] nums, List<Integer> paths, List<List<Integer>> ans){
-            if (paths.size() == nums.length){
+        private void myMethod(int[] nums, List<Integer> paths, List<List<Integer>> ans) {
+            if (paths.size() == nums.length) {
                 ans.add(new ArrayList<>(paths));
             }
             for (int i = 0; i < nums.length; i++) {
-                if (!exist(nums[i], paths)){
+                if (!exist(nums[i], paths)) {
                     paths.add(nums[i]);
                     myMethod(nums, paths, ans);
                     paths.remove(paths.size() - 1);
@@ -75,13 +76,33 @@ public class Permutations {
 
         private boolean exist(int num, List<Integer> paths) {
             for (Integer path : paths) {
-                if (path == num){
+                if (path == num) {
                     return true;
                 }
             }
             return false;
         }
 
+        // 可以对数组分区进行优化，当前遍历的元素左侧是已选元素
+        private void optimizeMyMethod(int[] nums, List<Integer> paths, List<List<Integer>> ans, int start) {
+            if (paths.size() == nums.length) {
+                ans.add(new ArrayList<>(paths));
+                return;
+            }
+            for (int i = start; i < nums.length; i++) {
+                paths.add(nums[i]);
+                swap(nums, i, start);
+                optimizeMyMethod(nums, paths, ans, start + 1);
+                swap(nums, i, start);
+                paths.remove(paths.size() - 1);
+            }
+        }
+
+        private void swap(int[] nums, int a, int b) {
+            int temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
+        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
